22090c3939b9bcdfa32aee279ebc432e01fd4dc8
Avram Adrian-Constantin <aditaavram@yahoo.com>
2021-05-22 17:42:45 +0300

    Update

--- src/endpoints/SurfaceApi.cpp
+++ src/endpoints/SurfaceApi.cpp
@@ -21,7 +21,7 @@ void SurfaceApi::handle(const Pistache::Rest::Request &request, Pistache::Http::
 
     auto cameraMatrixMat = this->fromStringToMatrix(cameraMatrix->getContent(), 3, 3);
     
-    std::vector<std::tuple<cv::Mat, cv::Mat, cv::Mat, cv::Mat, cv::Mat>> imagesToProcess;
+    std::cv::Mat imageToProcess;
 
     for (int i = 0; i < std::atoi(n->getContent().c_str()); i++) {
         auto image = bodyFormParser.get("image" + std::to_string(i));
@@ -40,27 +40,24 @@ void SurfaceApi::handle(const Pistache::Rest::Request &request, Pistache::Http::
 
         spdlog::info("handle() - image {} height: {} width: {}", i, imageDecoded.rows, imageDecoded.cols);
 
-        auto calibratedImage = this->calibrate(imageDecoded, imagePoints2D, objectPoints3D,
-                                               cameraMatrixMat, distCoefficientsMat,
-                                               rotationVector, translationVector, true);
+     
 
-        imagesToProcess.push_back(std::make_tuple(calibratedImage, cameraMatrixMat, distCoefficientsMat, rotationVector,
-                                                  translationVector));
+        imageToProcess=cameraMatrixMat;
     }
 
-    float surface = calculateSurface(imagesToProcess, 20000, 10, 30, 10, 30, 0, 28);
+    float surface = calculateSurface(imageToProcess, 20000, 10, 30, 10, 30, 0, 28);
 
     spdlog::info("handle() - surface: {}", surface);
 
     response.send(Pistache::Http::Code::Ok, std::to_string(surface));
 }
 
-float_t SurfaceApi::calculateSurface(std::vector<std::tuple<cv::Mat, cv::Mat, cv::Mat, cv::Mat, cv::Mat>> images,
+float_t SurfaceApi::calculateSurface(std::cv::Mat image,
                                    uint32_t numberOfPoints, uint32_t xBoxRangeFrom, uint32_t xBoxRangeTo,
                                    uint32_t yBoxRangeFrom, uint32_t yBoxRangeTo
                                    ) {
 
-    std::vector<std::vector<cv::Point2f>> points(images.size(), std::vector<cv::Point2f>(numberOfPoints));
+    std::vector<cv::Point2f> points(std::vector<cv::Point2f>(numberOfPoints));
     for (int i = 0; i < images.size(); i++) {
         for (int j = 0; j < numberOfPoints; j++) {
             auto tempPoints = this->randomPoint2D(xBoxRangeFrom, xBoxRangeTo, yBoxRangeFrom, yBoxRangeTo);
@@ -76,16 +73,12 @@ float_t SurfaceApi::calculateSurface(std::vector<std::tuple<cv::Mat, cv::Mat, cv
     // walk to all points
     for (uint32_t k = 0; k < numberOfPoints; k++) {
         int8_t isValidPoint = 1;
-        // walk to all images and check if the point is white in all of them
-        for (int j = 0; j < images.size(); j++) {
-
-            // cv::Point2f projectedPoint = this->projectPoint(images[j], points[j][k]);
+       
             
-            cv::Mat image = std::get<0>(images[j]);
-            if (image.at<int>(points[j][k].x, points[j][k].y) != 0) {
+            if (image.at<int>(points[k].x, points[k].y) != 0) {
                 isValidPoint *= 0;
             }
-        }
+        
         if(isValidPoint==1){
             if(points[j][k].y>northernPoint)
                 northernPoint=points[j][k].y;
